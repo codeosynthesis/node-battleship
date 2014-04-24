093c404b2935cc51aeb54be7301b3dec5197c4e2
Kevin <kah3725@rit.edu>
2014-04-24 00:27:47 -0400

    Fire.js Added

--- routes/fire.js
+++ routes/fire.js
@@ -6,14 +6,35 @@
 exports.fire = function(req, res){
     //req.user //req.x req.y
     
+
+    var val = new Validation()
+    if (val.isUserIdValid(req.params.player) === false || val.isWithBounds(req.params.y) === false || val.isWithBounds(req.params.x) === false) {
+    	res.send('Invalid Params');
+    }
     var ret='user = '+req.params.player;
     ret+='x = '+req.params.x;
     ret+='y = '+req.params.y;
-    ret+=global.clients;
+    ret+= global.clients;
     res.send(ret);
-    /*
-        if user = player1
-        clients[1].board.checkHit(req.x,req.y)
-    */
 
+};
+
+var Validation = function() {
+
+}
+
+Validation.prototype.isWithBounds = function(x) {
+	if(x > 10){
+		return false;
+	}else{
+		return true;
+	}
+}
+
+Validation.prototype.isUserIdValid = function(y) {
+	if (y === 'player1' || y === 'player2') {
+		return true
+	} else{
+		return false;
+	}
 };
